cb6923bdfde1295056e16858ff140e1873a5bb34
daw66 <daw66@aber.ac.uk>
2021-06-11 15:15:18 +0100

    Open resoruces and more controllers to dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module Inventory.Caretaker {
+module inventory.caretaker {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.hibernate.orm.core;
@@ -9,7 +9,16 @@ module Inventory.Caretaker {
     requires java.xml.bind;
 
     opens uk.ac.aber.cs39440.inventory.caretaker.ui.controller to javafx.fxml;
+    opens uk.ac.aber.cs39440.inventory.caretaker.ui.controller.supplier to javafx.fxml;
+    opens uk.ac.aber.cs39440.inventory.caretaker.ui.controller.category to javafx.fxml;
+    opens uk.ac.aber.cs39440.inventory.caretaker.ui.controller.grid to javafx.fxml;
+    opens uk.ac.aber.cs39440.inventory.caretaker.ui.controller.stock to javafx.fxml;
     opens uk.ac.aber.cs39440.inventory.caretaker.data.entity to org.hibernate.orm.core;
+    opens css;
+    opens images.system;
+    opens images.supplier;
+    opens images.category;
+    opens images.stock;
 
     exports uk.ac.aber.cs39440.inventory.caretaker.application;
 }
